b95b4129dde8468c9f1c61403cc15bf63191188c
Carlos <carlos.e.santaniello@gmail.com>
2020-12-06 18:06:42 +0100

    Ciclo For

--- ejemplo2.php
+++ ejemplo2.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ejemplo 1</title>
+    <title>If-Else</title>
 </head>
 <body>
     <?php
---
+++ ejemplo3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>For</title>
+</head>
+<body>
+    <?php
+
+        $num1 = 5;
+        for($i = 0; $i <= 10; $i++){
+            $total = $num1 * $i;
+            echo "$num1 x $i = <b>$total</b><br>";
+        }
+    ?>
+</body>
+</html>
